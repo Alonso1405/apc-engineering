95db95b0585f27c2e18a9cc94381d721683a7a24
Alonso1405 <31044403+Alonso1405@users.noreply.github.com>
2026-06-17 18:04:05 -0600

    Agregar soporte PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "FEDRA Lubricantes",
+        short_name: "FEDRA",
+
+        description: "Distribuidor autorizado Petro-Canada Lubricants",
+
+        theme_color: "#dc2626",
+        background_color: "#ffffff",
+
+        display: "standalone",
+
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
